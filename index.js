113a8fbcbe567b1cbfab3359392b22c72d01a48c
LAPTOP-NR7UGA8K\jumadi <jumadi59.jj@gmail.com>
2022-05-28 12:48:47 +0700

    fix error

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 5000
+const data = require('./mock/data.json')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index')).use(bodyParser.json()).get('/list', (req, res) => {
+  .get('/', (req, res) => res.render('pages/index'))
+  .use(bodyParser.json())
+  .get('/list', (req, res) => {
     res.send({
       message: 'TIKET KERETA API',
       status: "SUCCESS",
